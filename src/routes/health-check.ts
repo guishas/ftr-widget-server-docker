7f567ea74d03c3767df242941dfb88d37470fd48
Guilherme Lunetta <lunettagui@gmail.com>
2025-05-12 19:53:54 -0300

    configure role arn in app runner deploy gh actions

--- src/routes/health-check.ts
+++ src/routes/health-check.ts
@@ -2,6 +2,6 @@ import type { FastifyInstance } from "fastify";
 
 export async function healthCheckRoute(app: FastifyInstance) {
   app.get('/health', async (request, reply) => {
-    await reply.status(201).send({ message: 'OK!' })
+    await reply.status(201).send({ message: 'Tudo OK!' })
   })
 }
